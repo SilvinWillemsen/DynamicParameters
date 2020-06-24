183091266357623e151b70e2f9d6f3705f432de6
SilvinWillemsen <silvinwillemsen@gmail.com>
2020-06-24 11:01:20 +0200

    Working energy half boundary

--- Matlab/dynamicBoundary.m
+++ Matlab/dynamicBoundary.m
@@ -2,13 +2,9 @@ clear all;
 close all;
 clc;
 
-upsample = 4;
-
 drawSpeed = 1;
 fs = 44100;
-fs1 = fs * upsample;
 k = 1/fs;
-k1 = 1/fs1;
 
 lengthSound = fs*2;
 
@@ -21,8 +17,6 @@ else
 end
 
 h = 1/N;
-h1 = 1/(N*upsample);
-
 c = h/k;
 
 rho = 7850;
@@ -32,35 +26,21 @@ A = pi * r^2;
 Tinit = c^2 * rho * A;
 
 h = c*k;
-h1 = c*k1;
-
 N = floor(1/h);
-N1 = floor(1/h1);
-
 h = 1/N;
-h1 = 1/N1;
 
 lambdaSq = (c*k/h)^2
 
 uNext = zeros(N+1, 1);
 u = zeros(N+1, 1);
-loc = 3/4
+loc = 3/4;
 hannRange = floor(N*loc - N/12):floor(N*loc + N/12);
 % u(floor(N/2)) = 1;
 u(hannRange) = hann(length(hannRange));
 % u(floor(N/2)) = 1;
 uPrev = u;
 
-u1Next = zeros(N1+1, 1);
-u1 = zeros(N1+1, 1);
-hannRange1 = floor(N1*loc - N1/12):floor(N1*loc + N1/12);
-u1(hannRange1) = hann(length(hannRange1));
-% u1(floor(N1/2)) = 1;
-
-u1Prev = u1;
-
 origHLocs = 0:h:1-h;
-origH1Locs = 0:h1:1-h1;
 
 flag = false;
 changeT = true;
@@ -78,20 +58,14 @@ for n = 1:lengthSound
     
     c = sqrt(T / (rho * A));
     h = c * k;
-%     h1 = c * k1;
     
     N = floor(1/h);
-%     N1 = floor(1/h1);
     
     hSave(n) = h;
     hLocs = 1:-h:0;
     
-%     h1Save(n) = h1;
-%     h1Locs = 1:-h1:0;
-
     lambdaSq =  c^2 * k^2 / h^2;
     range = 2:N;
-%     range1 = 2:N1;
 
     if abs(N - NPrev) > 1
         disp('too fast')
@@ -100,18 +74,12 @@ for n = 1:lengthSound
         uNext = [0;uNext];
         u = [0;u];
         uPrev = [0;uPrev];
-%         u1Next = [0;u1Next];
-%         u1 = [0;u1];
-%         u1Prev = [0;u1Prev];
     end
     
     if N < NPrev
         uNext = uNext(2:end);
         u = u(2:end);
         uPrev = uPrev(2:end);
-%         u1Next = u1Next(2:end);
-%         u1 = u1(2:end);
-%         u1Prev = u1Prev(2:end);
     end
       
     if hLocs(end) >= hSave(n)/2
@@ -130,35 +98,25 @@ for n = 1:lengthSound
     %% full string
     uNext(range) = (2-2*lambdaSq) * u(range) + lambdaSq * (u(range+1) + u(range-1)) - uPrev(range);
     uNext(1) = 2 * u(1) - uPrev(1) + lambdaSq * (u(2) - 2 * u(1) + uVirtual);
-%     for m = 1:upsample
-%         u1Next(range1) = (2-2*lambdaSq) * u1(range1) + lambdaSq * (u1(range1+1) + u1(range1-1)) - u1Prev(range1);
-%  
-%         kinEnergy1 = 1 / 2 * h1 * sum((1/k1 * (u1 - u1Prev)).^2);
-%         potEnergy1 = c^2 / (2 * h1) * sum((u1(range1) - u1(range1-1)) .* (u1Prev(range1) - u1Prev(range1-1)));
-%         totEnergy1 = [totEnergy1; kinEnergy1+potEnergy1];
-%         if m == 1
-%             u1Plot = u1Next;
-%         end
-%         u1Prev = u1;
-%         u1 = u1Next;
-%     end    
-%     
-    rOCpotRange = 2:N;
+
     kinEnergy(n) = rho * A / 2 * h * sum((1/k * (u(range) - uPrev(range))).^2);
     kinEnergyBound(n) = rho * A / 2 * h * (1/k * (u(1) - uPrev(1)))^2;
 
 %     kinEnergy(n) = rho * A / 2 * h * sum((1/k * (u - uPrev)).^2);
     potEnergy(n) = T / (2 * h) * sum((u(2:N+1) - u(1:N)) .* (uPrev(2:N+1) - uPrev(1:N)));
+    potEnergyBound(n) = 2 * T / (2 * h) * sum((u(1) - 0) .* (uPrev(1) - 0));
     scaling = ones(N+1, 1);
     scaling(1) = 0.5;
-    rOCkinEnergy(n) = h * rho * A / (2 * k^3) * sum(scaling .* (uNext - 2 * u + uPrev) .* (uNext - uPrev));
-    rOCpotEnergy(n) = h * T / (2*k*h^2) * sum((u(rOCpotRange+1) - 2 * u(rOCpotRange) + u(rOCpotRange-1))...
-        .* (uNext(rOCpotRange) - uPrev(rOCpotRange)));
-    boundaryEnergy(n) = -T / (4 * k * h)  * (uNext(1) - uPrev(1)) * (u(1) + u(2));
     
-    totEnergy(n) = kinEnergy(n) + kinEnergyBound(n) + potEnergy(n);
+%     rOCpotRange = 2:N;
+%     rOCkinEnergy(n) = h * rho * A / (2 * k^3) * sum(scaling .* (uNext - 2 * u + uPrev) .* (uNext - uPrev));
+%     rOCpotEnergy(n) = h * T / (2*k*h^2) * sum((u(rOCpotRange+1) - 2 * u(rOCpotRange) + u(rOCpotRange-1))...
+%         .* (uNext(rOCpotRange) - uPrev(rOCpotRange)));
+%     boundaryEnergy(n) = -T / (4 * k * h)  * (uNext(1) - uPrev(1)) * (u(1) + u(2));
+%     
+    totEnergy(n) = kinEnergy(n) + kinEnergyBound(n) + potEnergy(n) + potEnergyBound(n);
     
-    totROCEnergy(n) = rOCkinEnergy(n) - rOCpotEnergy(n) - boundaryEnergy(n);
+%     totROCEnergy(n) = rOCkinEnergy(n) - rOCpotEnergy(n) - boundaryEnergy(n);
     
     if mod(n, drawSpeed) == 0
 
