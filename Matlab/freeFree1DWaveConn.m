6e7865abd63e0ec494bb2cc21a813251c4d0494f
SilvinWillemsen <silvinwillemsen@gmail.com>
2020-08-03 09:17:12 +0200

    Semi-working free-free connection (just not the right frequencies yet)

--- Matlab/freeFree1DWaveConn.m
+++ Matlab/freeFree1DWaveConn.m
@@ -2,177 +2,155 @@ clear all;
 close all;
 clc;
 
-drawSpeed = 10;
+drawSpeed = 10000;
 fs = 44100;
 k = 1/fs;
-lengthSound = fs*2;
+lengthSound = fs;
 
-N = 50.25;
-
+N = 29.75;
+L = 1;
 h = 1/N;
 
-c = h/k;
-rho = 7850;
-Einit = c^2 * rho;
+cInit = h/k;
+c = cInit;
 
 h = c*k;
 N = floor(1/h);
 h = 1/N;
 lambdaSq = (c*k/h)^2
 
-u1Next = zeros(N+1, 1);
-u1 = zeros(N+1, 1);
-u1(floor(N/4)-2:floor(N/4)+2) = hann(5);
-u1Prev = u1;
+uLeftNext = zeros(floor(N/2) + 1, 1);
+uLeft = zeros(floor(N/2) + 1, 1);
 
-u2Next = zeros(floor(N/2) + 1, 1);
-u2 = zeros(floor(N/2) + 1, 1);
-u2(floor(N/4)-2:floor(N/4)+2) = hann(5);
-u2Prev = u2;
+if mod(N,2) == 1
+    uLeftNext = [uLeftNext; 0];
+    uLeft = [uLeft; 0];
+end
 
-u3Next = zeros(floor(N/2) + 1, 1);
-u3 = zeros(floor(N/2) + 1, 1);
+uLeft(floor(N/4)-4:floor(N/4)+4) = hann(9);
+uLeftPrev = uLeft;
+
+uRightNext = zeros(floor(N/2) + 1, 1);
+uRight = zeros(floor(N/2) + 1, 1);
 % u3(floor(N/4)-5:floor(N/4)+5) = hann(11);
-u3Prev = u3;
+uRightPrev = uRight;
 
-uNext = zeros(N, 1);
-u = zeros(N, 1);
-u(floor(N/4)-2:floor(N/4)+2) = hann(5);
-uPrev = u;
-
-halfRange = 2:length(u2)-1;
+halfRange = 2:length(uLeft)-1;
 
 origHLocs = 0:h:1;
 
 flag = false;
-changeE = true;
-E = Einit;
+changeC = false;
 for n = 1:lengthSound
     NPrev = N;
-    if changeE
-        E = Einit * (1-0.5*n/lengthSound);
+    if changeC
+        c = cInit * (1-0.25*n/lengthSound);
     else
-        E = E;
+        c = c;
     end
-%     E = Einit; % just for now (energy)
-    K = E / h;
-    M = rho * h;
-    h = k * sqrt(E / rho);
+    h = c*k;
     N = floor(1/h);
+    Nsave(n) = N;
     hSave(n) = h;
     hLocs = 1:-h:0;
-%     if length(hLocs) > N
-%         hLocs = hLocs(1:N);
-%     end
-%     h = 1/N;
-    lambdaSq = E / rho * k^2 / h^2;
-    range = 2:N;
+    
+    lambdaSq = c^2 * k^2 / h^2;
 
     if abs(N - NPrev) > 1
         disp('too fast')
     end
     if N > NPrev
-        uNext = [0;uNext];
-        u = [0;u];
-        uPrev = [0;uPrev];
-        u1Next = [0;u1Next];
-        u1 = [0;u1];
-        u1Prev = [0;u1Prev];
+        if mod(N,2) == 1
+            uLeftNext = [uLeftNext; uRightNext(1)];
+            uLeft = [uLeft; uRight(1)];
+            uLeftPrev = [uLeftPrev; uRightPrev(1)];
+        else 
+            uRightNext = [uLeftNext(end);uRightNext];
+            uRight = [uLeft(end);uRight];
+            uRightPrev = [uLeftPrev(end);uRightPrev];
+        end
     end
     
     if N < NPrev
-        uNext = uNext(2:end);
-        u = u(2:end);
-        uPrev = uPrev(2:end);
-        u1Next = u1Next(2:end);
-        u1 = u1(2:end);
-        u1Prev = u1Prev(2:end);
+        if mod(N,2) == 0
+            uLeftNext = uLeftNext(1:end-1);
+            uLeft = uLeft(1:end-1);
+            uLeftPrev = uLeftPrev(1:end-1);
+        else 
+            uRightNext = uRightNext(2:end);
+            uRight = uRight(2:end);
+            uRightPrev = uRightPrev(2:end);
+        end
     end
-%     %% mass-spring string
-%     for l = 2:N-1
-%         uNext(l) = 2 * u(l) - uPrev(l) + k^2 * K/M * (u(l+1) - u(l)) - k^2 * K/M * (u(l) - u(l-1));
-%     end
-%         
-    if hLocs(end) >= hSave(n)/2
-        alpha = (hSave(n) - hLocs(end)) / hLocs(end);
-        u1Virtual = -alpha * u1(1); %0 = boundary;
-        flag = true;
-    else
-        alpha = (2*hLocs(end)) / hSave(n);
-        u1Virtual = -(alpha * u1(1) + (1-alpha) * u1(2));
-    end
-%         if flag && floor(alpha * 1000) == 0
-%             changeE = false;
-%         end
 
-%     u1Virtual = -tand(atand(u1(1)/(alpha*h)))*(1-alpha*h);
-%     u1Virtual = u1(2);
-%     if u1Virtual ~= 0
-%         disp("wait")
-%     end
-    %% full string
-    u1Next(range) = (2-2*lambdaSq) * u1(range) + lambdaSq * (u1(range+1) + u1(range-1)) - u1Prev(range);
-%     u1Next(1) = (2 * u1(1) - u1Prev(1) + 2 * lambdaSq * (u1(2) - u1(1)) + alpha * omega0^2 * k^2 / (2 * h) * uPrev(1) - (1-alpha) * lambdaSq * u1(2)) / (1 + alpha * omega0^2 * k^2 / (2 * h));
-    u1Next(1) = 2 * u1(1) - u1Prev(1) + lambdaSq * (u1(2) - 2 * u1(1) + u1Virtual);
     
-    kinEnergy(n) = 1 / 2 * h * sum((1/k * (u1 - u1Prev)).^2);
-    potEnergy(n) = c^2 / (2 * h) * sum((u1(2:N+1) - u1(1:N)) .* (u1Prev(2:N+1) - u1Prev(1:N)));
+    leftRange = 2:length(uLeft)-1;
+    rightRange = 2:length(uRight)-1;
+    if mod(N,2) == 1
+        hLocsLeft = 0:h:((L + h) /2);
+        hLocsRight = flip(L:-h:(L/2));
+    else
+        hLocsLeft = 0:h:(L/2);
+        hLocsRight = flip(L:-h:((L - h * 0.9999999)/2));
+    end
+    alf = (hLocsRight(1) - hLocsLeft(end)) / h;
+    uLeftPoint = alf * uLeft(end) + (1 - alf) * uLeft(end-1);
+    uRightPoint = alf * uRight(1) + (1 - alf) * uRight(2);
+
+    %% left half string
+    uLeftNext(leftRange) = (2-2*lambdaSq) * uLeft(leftRange) + lambdaSq * (uLeft(leftRange+1) + uLeft(leftRange-1)) - uLeftPrev(leftRange);
+%     uLeftNext(end) = (2-2*lambdaSq) * uLeft(end) + lambdaSq * (2 * uLeft(end-1)) - uLeftPrev(end);
+    uLeftNext(end) = (2-2*lambdaSq) * uLeft(end) + lambdaSq * (uLeft(end-1) + uRightPoint) - uLeftPrev(end);
     
-    totEnergy(n) = kinEnergy(n) + potEnergy(n);
-%     %% left half string
-%     u2Next(halfRange) = (2-2*lambdaSq) * u2(halfRange) + lambdaSq * (u2(halfRange+1) + u2(halfRange-1)) - u2Prev(halfRange);
-%     u2Next(end) = (2-2*lambdaSq) * u2(end) + lambdaSq * (2 * u2(end-1)) - u2Prev(end);
+    %% right half string
+    uRightNext(rightRange) = (2-2*lambdaSq) * uRight(rightRange) + lambdaSq * (uRight(rightRange+1) + uRight(rightRange-1)) - uRightPrev(rightRange);
+%     uRightNext(1) = (2-2*lambdaSq) * uRight(1) + lambdaSq * (2 * uRight(2)) - uRightPrev(1);
+    uRightNext(1) = (2-2*lambdaSq) * uRight(1) + lambdaSq * (uRight(2) + uLeftPoint) - uRightPrev(1);
+
+    %% connection
+%     F = h * lambdaSq * (uRight(2) - uLeft(end-1));
 %     
-%     %% right half string
-%     u3Next(halfRange) = (2-2*lambdaSq) * u3(halfRange) + lambdaSq * (u3(halfRange+1) + u3(halfRange-1)) - u3Prev(halfRange);
-%     u3Next(1) = (2-2*lambdaSq) * u3(1) + lambdaSq * (2 * u3(2)) - u3Prev(1);
-%         
-%     %% connection
-%     F = h * (u3Next(1) - u2Next(end)) / 2;
-%     
-%     u2Next(end) = u2Next(end) + 1/h * F;
-%     u3Next(1) = u3Next(1) - 1/h * F;
-    
+%     uLeftNext(end) = uLeftNext(end) + 1/h * F;
+%     uRightNext(1) = uRightNext(1) - 1/h * F;
+
     if mod(n, drawSpeed) == 0
-%         hold off;
-%         scatter(flip(hLocs), zeros(length(hLocs),1));
-%         hold on;
-%         scatter(origHLocs, zeros(length(origHLocs),1));
-        subplot(2,1,1)
-%         plot(uNext, 'o-');
-%         subplot(2,1,2)
+
+        
         hold off;
-        plot([hLocs(end)-hSave(n), flip(hLocs)], [u1Virtual; u1], 'o-')
-        ylim([-1,1])
+        plot(hLocsLeft * N, uLeft, 'LineWidth' ,2, 'Marker', '.', 'MarkerSize', 20, 'Color', 'b') 
+%         plot([hLocsLeft, hLocsLeft(end) + h] * N, [0;0;0;0;0;0;-5.55111512312578e-17;-0.146446609406726;-0.353553390593274;-0.500000000000000;-0.500000000000000;-0.353553390593274;-0.146446609406726;0;0;0;0], 'LineWidth' , 2, 'Marker', '.', 'MarkerSize', 20)
         hold on;
-        scatter(origHLocs, zeros(length(origHLocs),1));
-        scatter(0, 0, 180, 'k', 'x')
-        scatter(h-hLocs(end), -u1Virtual, 18, 'k', 'o')
-        plot([hLocs(end) - hSave(n), h-hLocs(end)], [u1Virtual, -u1Virtual])
-%         subplot(3,1,3)
-%         plot(flip(hLocs), [u2Next;u3Next], 'o-')
-    xlim([-1.5*h, 4*h])
-    pause (0.25)
-        subplot(2,1,2)
-        plot(totEnergy(1:n) / totEnergy(1) - 1)
+        plot(hLocsRight * N, uRight, 'Linewidth', 2,  'Marker', 'o', 'MarkerSize', 10, 'Color', 'r')
+%         plot(hLocsRight * N, [0;5.55111512312578e-17;0;0;0.146446609406726;0.353553390593274;0.500000000000000;0.500000000000000;0.353553390593274;0.146446609406726;0;0;0;0;0;0], 'Linewidth', 2,  'Marker', 'o', 'MarkerSize', 10)
+        ylim([-0.6, 0.6])
+        grid on;
+%         hold on;
+%         scatter([hLocsLeft(end) + h, hLocsRight(1) - h], [uRightPoint, uLeftPoint]);
+%         scatter((hLocsLeft(end)+h) * N, 0, 'b', 'Marker', 'o', 'Linewidth', 2)
+%         scatter((hLocsRight(1)-h) * N, 0, 'r', 'filled')
+%         legend('$u$', '$w$', '$u_{M+1}$', '$w_{-1}$', 'interpreter', 'latex', 'Fontsize', 16)
+        legend('$u$', '$w$', 'interpreter', 'latex', 'Fontsize', 16)
+
+        set(gca, 'Fontsize', 16, 'Linewidth', 2)
+        if n == 16
+            disp("wait")
+        end
         drawnow;
     end
-    uPrev = u;
-    u = uNext;
+    uLeftPrev = uLeft;
+    uLeft = uLeftNext;
     
-    u1Prev = u1;
-    u1 = u1Next;
+    uRightPrev = uRight;
+    uRight = uRightNext;
     
-    u2Prev = u2;
-    u2 = u2Next;
-    
-    u3Prev = u3;
-    u3 = u3Next;
-    out1(n) = u1Next(floor(N - 10));
-    out(n) = uNext(floor(N - 10));
-%     sum(u1-[u2;u3(2:end)])
+    outFree(n) = uRightNext(end - 10);
 end
 subplot(2,1,1)
-plot(out)
+plot(outFree)
+
 subplot(2,1,2)
-plot(out1)
+outfft = fft(outFree);
+semilogy([0:lengthSound-1]'*fs/lengthSound, abs(outfft), 'r');
+xlim([0 3*c/L])
+
+c/2
