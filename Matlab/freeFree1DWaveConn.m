9e004fc7358ed7eecf7dc3737db100ecb4303768
SilvinWillemsen <silvinwillemsen@gmail.com>
2020-08-11 09:24:28 +0200

    Semi overlapping double points. Now going back to <1 overlapping point

--- Matlab/freeFree1DWaveConn.m
+++ Matlab/freeFree1DWaveConn.m
@@ -2,12 +2,12 @@ clear all;
 close all;
 clc;
 
-drawSpeed = 10000;
+drawSpeed = 1;
 fs = 44100;
 k = 1/fs;
 lengthSound = fs;
 
-Ninit = 30.5;
+Ninit = 30.0;
 N = Ninit;
 L = 1;
 h = 1/N;
@@ -17,11 +17,14 @@ c = cInit;
 
 h = c*k;
 N = floor(1/h);
-h = 1/N;
+% h = 1/N;
 lambdaSq = (c*k/h)^2
 
 overlap = 2;
 
+alf = Ninit - N;
+
+%% initialise states
 uNext = zeros(floor(N/2) + ceil((overlap + 1) / 2) - 1, 1);
 u = zeros(floor(N/2) + ceil((overlap + 1) / 2) - 1, 1);
 
@@ -30,18 +33,18 @@ if mod(N,2) == 1
     u = [u; 0];
 end
 
-u(floor(N/5)-4:floor(N/5)+4) = hann(9);
+u(floor(N/5)-4:floor(N/5)+4) = hann (9);
 uPrev = u;
 
 wNext = zeros(floor(N/2) + floor((overlap + 1) / 2) - 1, 1);
 w = zeros(floor(N/2) + floor((overlap + 1) / 2) - 1, 1);
-% u3(floor(N/4)-5:floor(N/4)+5) = hann(11);
-% w(floor(2*N/5)-4:floor(2*N/5)+4) = hann(9);
 wPrev = w;
 
-zNext = zeros(N, 1);
-z = zeros(N-1, 1);
-z(floor(N/5)-4:floor(N/5)+4) = hann(9);
+zNext = zeros(N*2-1, 1);
+z = zeros(N*2-1, 1);
+% z(floor(N*2/5)-8:2:floor(N*2/5)+8) = u(floor(N/5)-4:floor(N/5)+4);
+% z(floor(N*2/5)-7:2:floor(N*2/5)+7) = 0.5 * (u(floor(N/5)-3:floor(N/5)+4) + u(floor(N/5)-4:floor(N/5)+3));
+z(floor(N*2/5)-8:floor(N*2/5)+8) = hann (17);
 zPrev = z;
 
 origHLocs = 0:h:1;
@@ -49,139 +52,47 @@ origHLocs = 0:h:1;
 eu = ones(length(u), 1);
 Dxxu = spdiags([eu -2*eu eu], -1:1, length(u),length(u));
 
-Dxxu(end-1,end-2) = 1.5;
-Dxxu(end,end-1) = 1.5;
-
+Dxxu(end-1,end-2) = 2 - alf;
+Dxxu(end,end-1) = 1 + alf;
 
 ew = ones(length(w), 1);
 Dxxw = spdiags([ew -2*ew ew], -1:1, length(w),length(w));
-% Dxxw(3,2) = 0.5;
-Dxxw(2,3) = 1.5;
-Dxxw(1,2) = 1.5;
-% Dxxw(2,1) = 1/3;
-% Dxxw(1,1) = -2;
+Dxxw(2,3) = 2 - alf;
+Dxxw(1,2) = 1 + alf;
 
 ez = ones(length(z), 1);
 Dxxz = spdiags([ez -2*ez ez], -1:1, length(z),length(z));
 
-flag = false;
-changeC = false;
-
-eta = 0;
-etaNext = 0;
-omega0 = 10000;
 interpol = "linear";
 outFree = zeros(lengthSound, 1);
-for n = 1:lengthSound
-    NPrev = N;
-    if changeC
-        c = cInit * (1-5*n/lengthSound);
-    else
-        c = c;
-    end
-    h = 1.001*c*k;
-    Ninit = 1/h;
-    N = floor(1/h);
-    Nsave(n) = N;
-    hSave(n) = h;
-    hLocs = 1:-h:0;
-    
-    lambdaSq = c^2 * k^2 / h^2;
-    hLocsLeft = (1:(length(u))) * h;
-    hLocsRight = flip(1 - ((1:(length(w))) * h));
 
-    alf = (Ninit - N) + 0.5 * (1-(Ninit - N));
-%     alf = 1 - 0.5 * (hLocsLeft(floor(N/2)) - hLocsRight(length(hLocsRight) - floor(N/2) + 1)) / h;
-  
-    if abs(N - NPrev) > 1
-        disp('too fast')
-    end
-    if N > NPrev
-        if mod(N,2) == 1
-            if interpol == "linear"
-                uNext = [uNext; ((1-alf) * wNext(1) + alf * wNext(2) - alf * uNext(end)) / (1-alf)];
-                u = [u; ((1-alf) * w(1) + alf * w(2) - alf * u(end)) / (1-alf)];
-                uPrev = [uPrev; ((1-alf) * wPrev(1) + alf * wPrev(2) - alf * uPrev(end)) / (1-alf)];
-            else
-                uNext = [uNext; ((1-alf) * wNext(1) + alf * wNext(2) - alf * uNext(end)) / (1-alf)];
-                u = [u; ((1-alf) * w(1) + alf * w(2) - alf * u(end)) / (1-alf)];
-                uPrev = [uPrev; ((1-alf) * wPrev(1) + alf * wPrev(2) - alf * uPrev(end)) / (1-alf)];
-            
-            end
-        else 
-            wNext = [uNext(end - overlap + 1);wNext];
-            w = [u(end - overlap + 1);w];
-            wPrev = [uPrev(end - overlap + 1);wPrev];
-        end
-        eu = ones(length(u), 1);
-        Dxxu = spdiags([eu -2*eu eu], -1:1, length(u),length(u));
-        Dxxu(end, end - 1) = 2;
-        ew = ones(length(w), 1);
-        Dxxw = spdiags([ew -2*ew ew], -1:1, length(w),length(w));
-        Dxxw(1, 2) = 2;
-    end
-    
-    if N < NPrev
-        if mod(N,2) == 0
-            uNext = uNext(1:end-1);
-            u = u(1:end-1);
-            uPrev = uPrev(1:end-1);
-        else 
-            wNext = wNext(2:end);
-            w = w(2:end);
-            wPrev = wPrev(2:end);
-        end
-        eu = ones(length(u), 1);
-        Dxxu = spdiags([eu -2*eu eu], -1:1, length(u),length(u));
-        Dxxu(end, end - 1) = 2;
-        ew = ones(length(w), 1);
-        Dxxw = spdiags([ew -2*ew ew], -1:1, length(w),length(w));
-        Dxxw(1, 2) = 2;
-    end
-
-    
-    leftRange = 2:length(u)-1;
-    rightRange = 2:length(w)-1;
-%     if mod(N,2) == 1
-% %         hLocsLeft = 0:h:((L + h * 0.99999) /2);
-% %         hLocsRight = flip(L:-h:(L/2));
-%         hLocsLeft = 0:h:(L/2 + h * (overlap + 0.9999));
-%         hLocsRight = flip(L:-h:(L/2 - h * overlap));
-% 
-%     else
-%         hLocsLeft = 0:h:(L/2 + h * overlap);
-%         hLocsRight = flip(L:-h:(L/2 - h * (overlap)));
-%     end
-    hLocsLeft = (1:(length(u))) * h;
-    hLocsRight = flip(1 - ((1:(length(w))) * h));
-
-
-    cub = [alf * (alf - 1) * (alf - 2) / -6, ...
+%% connection  
+cub = [alf * (alf - 1) * (alf - 2) / -6, ...
             (alf - 1) * (alf + 1) * (alf - 2) / 2, ...
             alf * (alf + 1) * (alf - 2) / -2, ...
             alf * (alf + 1) * (alf - 1) / 6];
         
-    Iu = zeros(1, length(u));
-    Iw = zeros(1, length(w));
-    if interpol == "linear"
-        Iu(end) = alf;
-        Iu(end-1) = 1-alf;
-        Iw(2) = 1-alf;
-        Iw(1) = alf;
-    else
-        Iu(end-3 : end) = cub;
-        Iw(1:4) = cub;
-    end
-    Ju = Iu' * 1/h;    
-    Jw = Iw' * 1/h;
+I1u = zeros(1,length(u));
+I1w = zeros(1,length(w));
+I1u(end-1) = 1-alf;
+I1u(end) = alf;
+I1w(1) = 1;
+J1u = I1u' * 1/h;    
+J1w = I1w' * 1/h;
 
+I2u = zeros(1,length(u));
+I2w = zeros(1,length(w));
+I2u(end) = 1;
+I2w(1) = alf;
+I2w(2) = 1-alf;
+J2u = I2u' * 1/h;    
+J2w = I2w' * 1/h;
+
+for n = 1:lengthSound  
+    
+    hLocsLeft = (1:(length(u))) * h;
+    hLocsRight = flip(1 - ((1:(length(w))) * h));
         
-    uLeftLocs = ((length(uNext) - overlap + 1) : length(uNext))';
-    uRightLocs = (1:overlap)';
-    
-    uLeftPoints = alf * u(uLeftLocs) + (1 - alf) * u(uLeftLocs - 1);
-    uRightPoints = alf * w(uRightLocs) + (1 - alf) * w(uRightLocs + 1);
-    
     %% left half string
     uNext = 2 * u + lambdaSq * Dxxu * u - uPrev;
     
@@ -189,42 +100,24 @@ for n = 1:lengthSound
     wNext = 2 * w + lambdaSq * Dxxw * w - wPrev;
     
     %% orig string
-    zNext = 2 * z + lambdaSq * Dxxz * z - zPrev;
-
-
-    alf = 0.5;
-    %% connection  
-    I1u = zeros(1,length(u));
-    I1w = zeros(1,length(w));
-    I1u(end-1) = 1-alf;
-    I1u(end) = alf;
-    I1w(1) = 1;
-    J1u = I1u' * 1/h;    
-    J1w = I1w' * 1/h;
-
-    I2u = zeros(1,length(u));
-    I2w = zeros(1,length(w));
-    I2u(end) = 1;
-    I2w(1) = alf;
-    I2w(2) = 1-alf;
-    J2u = I2u' * 1/h;    
-    J2w = I2w' * 1/h;
-
-    
+    for i = 1:2
+        zNext = 2 * z + lambdaSq * Dxxz * z - zPrev;
+        zPrev = z;
+        z = zNext;
+    end
     a11 = I1u * J1u + I1w * J1w;
     a12 = I1u * J2u + I1w * J2w;
     a21 = I2u * J1u + I2w * J1w;
     a22 = I2u * J2u + I2w * J2w;
     
-%     F1 = h * c^2 / h^2 * (-u(end-2));
-%     F2 = h * c^2 / h^2 * (w(3));
-
     solut = [a11, a12; ...
              a21, a22] \ [(c^2 / h^2 * sum(I1w * Dxxw * w) - c^2 / h^2 * sum(I1u * Dxxu * u)); ...
                 (c^2 / h^2 * sum(I2w * Dxxw * w) - c^2 / h^2 * sum(I2u * Dxxu * u))];
     uNext = uNext + k^2 * (J1u * solut(1) + J2u * solut(2));
     wNext = wNext - k^2 * (J1w * solut(1) + J2w * solut(2));
 
+    %% energies
+    
     scalingU = ones(length(u),1);
     scalingU(end-1:end) = 0.5;
     kinEnergyU(n) = 1/2 * h * sum (scalingU .* (1/k * (u - uPrev)).^2);
@@ -250,37 +143,16 @@ for n = 1:lengthSound
         subplot(2,1,1)
         hold off;
         plot(hLocsLeft * N, u, 'LineWidth' ,2, 'Marker', '.', 'MarkerSize', 20, 'Color', 'b') 
-%         plot([hLocsLeft, hLocsLeft(end) + h] * N, [0;0;0;0;0;0;-5.55111512312578e-17;-0.146446609406726;-0.353553390593274;-0.500000000000000;-0.500000000000000;-0.353553390593274;-0.146446609406726;0;0;0;0], 'LineWidth' , 2, 'Marker', '.', 'MarkerSize', 20)
+
         hold on;
         wOffset = 0.00;
         plot(hLocsRight * N, w + wOffset, 'Linewidth', 2,  'Marker', 'o', 'MarkerSize', 10, 'Color', 'r')
-%         plot(z, 'Linewidth', 2,  'Marker', 'o', 'MarkerSize', 7, 'Color', 'g')
-%         plot(hLocsRight * N, [0;5.55111512312578e-1   7;0;0;0.146446609406726;0.353553390593274;0.500000000000000;0.500000000000000;0.353553390593274;0.146446609406726;0;0;0;0;0;0], 'Linewidth', 2,  'Marker', 'o', 'MarkerSize', 10)
+
+        plot((h/2:h/2:1-h/2) * N, z, 'Linewidth', 2,  'Marker', 'o', 'MarkerSize', 10)
+
+                
         ylim([-0.6, 0.6])
         grid on;
-% %         hold on;
-% %         scatter([hLocsLeft(end) + h, hLocsRight(1) - h], [uRightPoint, uLeftPoint]);
-% %         scatter((hLocsLeft(end)+h) * N, 0, 'b', 'Marker', 'o', 'Linewidth', 2)
-% %         scatter((hLocsRight(1)-h) * N, 0, 'r', 'filled')
-% %         legend('$u$', '$w$', '$u_{M+1}$', '$w_{-1}$', 'interpreter', 'latex', 'Fontsize', 16)
-%         textOffsetX = 0.1;
-%         xlim([12, 19])
-%         ylim([-0.1, 0.1] + wOffset * 0.5)
-%         textOffsetY = 0.007;
-%         text(14 + textOffsetX, -textOffsetY, '$u_{M-2}^n$', 'interpreter', 'latex', 'Fontsize', 18)
-%         text(15 + textOffsetX, -textOffsetY, '$u_{M-1}^n$', 'interpreter', 'latex', 'Fontsize', 18)
-%         text(16 + textOffsetX, -textOffsetY, '$u_M^n$', 'interpreter', 'latex', 'Fontsize', 18)
-%         text(15 + textOffsetX, wOffset + textOffsetY, '$w_0^n$', 'interpreter', 'latex', 'Fontsize', 18)
-%         text(16 + textOffsetX, wOffset + textOffsetY, '$w_1^n$', 'interpreter', 'latex', 'Fontsize', 18)
-%         text(17 + textOffsetX, wOffset + textOffsetY, '$w_2^n$', 'interpreter', 'latex', 'Fontsize', 18)
-%         
-%         plot([15, 15], [wOffset, 0], '--', 'Color', 'k', 'Linewidth', 2);
-%         plot([16, 16], [wOffset, 0], '--', 'Color', 'k', 'Linewidth', 2);
-% 
-%         text(15 + textOffsetX, wOffset*0.5, '$F_1$', 'interpreter', 'latex', 'Fontsize', 18)
-%         text(16 + textOffsetX, wOffset*0.5, '$F_2$', 'interpreter', 'latex', 'Fontsize', 18)
-% %         yticklabels([])
-%         yticks([])
 
         set(gca, 'Fontsize', 16, 'Linewidth', 2)
         if n == 16
@@ -289,18 +161,7 @@ for n = 1:lengthSound
         
         subplot(2,1,2)
         plot(totEnergy(1:n) / totEnergy(1) - 1)
-%         hold off
-%         plot(totEnergyU(1:n) + totEnergyW(1:n) - (totEnergyU(1) + totEnergyW(1)))     
-% %         hold on
-% %         plot(connEnergy(1:n))
-% %         hold off;
-%         
-% %         hold off;
-% %         plot(totEnergyU(1:n) / totEnergy(1) - 1);
-% %         hold on;
-% %         plot(connEnergy(1:n) / 2)
-%         subplot(3,1,3);
-%         plot(totEnergy(1:n) / totEnergy(1) - 1);
+        
         pause(0.2)
         drawnow;
     end
@@ -309,9 +170,6 @@ for n = 1:lengthSound
     
     wPrev = w;
     w = wNext;
-    
-    zPrev = z;
-    z = zNext;
     
     outFree(n) = wNext(end - 10);
 end
